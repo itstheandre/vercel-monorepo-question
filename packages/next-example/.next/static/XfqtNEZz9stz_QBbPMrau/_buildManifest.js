4fbac10acd598c530261d5230bdf8141affa000e
Andre <itstheandre@gmail.com>
2020-09-13 11:53:40 +0200

    vercel cli *issue* as an example

---
+++ packages/next-example/.next/static/XfqtNEZz9stz_QBbPMrau/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-e2935975f455ac2ab7bb.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-ddf38488177d602b7d7b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ packages/next-example/pages/index.js
@@ -0,0 +1,5 @@
+function Index() {
+  return <div>Hey there</div>;
+}
+
+export default Index;
